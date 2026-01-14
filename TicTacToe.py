8404735d05f8c94a5fa86ad7214af96685ee3604
EmmanCodes <22-03328@g.batstate-u.edu.ph>
2026-01-14 12:08:06 +0800

    Add AI opponent, enhanced board and added win functions
    Refactored Tic Tac Toe game to include AI opponent and improved board layout display.

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,5 +1,29 @@
+import random
+def BoardLayout(Board):
+    print(\
+        f"""
+        [{Board[0]}] [{Board[1]}] [{Board[2]}]
+        [{Board[3]}] [{Board[4]}] [{Board[5]}]
+        [{Board[6]}] [{Board[7]}] [{Board[8]}] """
+    )
+
+def Win(Board):
+    # All 8 ways to win
+    win_conditions = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8], # Rows
+        [0, 3, 6], [1, 4, 7], [2, 5, 8], # Columns
+        [0, 4, 8], [2, 4, 6]             # Diagonals
+    ]
+
+    for condition in win_conditions:
+        # Check if all three positions in the condition are the same and NOT empty
+        if Board[condition[0]] == Board[condition[1]] == Board[condition[2]] != " ":
+            return Board[condition[0]]  # Returns 'X' or 'O' as the winner
+
+    return None  # No winner yet
+
 def game():
-    board = [" ", " ", " ",
+    Board = [" ", " ", " ",
             " ", " ", " ",
             " ", " ", " "]
     
@@ -15,14 +39,57 @@ def game():
             Comp = "O"
             break
         else:
-            print("Stupid motherfucker")
+            print("Stupid motherfucker, Wrong icon")
 
-    print(\
-        f"""
-        [{board[0]}] [{board[1]}] [{board[2]}]
-        [{board[3]}] [{board[4]}] [{board[5]}]
-        [{board[6]}] [{board[7]}] [{board[8]}]
-        """
-    )
+    print("Game Start!")
+    BoardLayout(Board)
+    
+    GameRun = True
+
+    while GameRun:
+        #User turn
+        spot(Board, User, Comp)
+        if Win(Board):
+            print("User wins!")
+            GameRun = False
+            break
+        
+        if " " not in Board:
+            print("It's a DRAW!")
+            break
+
+        enemyspot(Board, User, Comp, location)
+        if Win(Board):
+            print("AI wins!")
+            GameRun = False
+            break
+
+
+def spot(Board, User, Comp):
+    while True:
+        global location
+        location = int(input("Where do you want to place your piece? "))
+        if location in range(0, 8):
+            if Board[location] == " ":
+                Board[location] = User
+                BoardLayout(Board)
+                break
+            else:
+                print("Choose another number")
+        else:
+            print("Stupid motherfucker")
+            
+
+def enemyspot(Board, User, Comp, location):
+    if " " in Board:
+        while True:
+            enemy = random.randint(0,8)
+
+            if Board[enemy] == " ":
+                Board[enemy] = Comp
+                BoardLayout(Board)
+                break
+    else:
+        Win()
 
 game()
